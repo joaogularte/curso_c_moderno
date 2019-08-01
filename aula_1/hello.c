b9e972e94ca68f10b200bbcbbb7e5cfaad1bbe5e
joaogularte <jov96@hotmail.com>
2019-08-01 06:46:06 -0300

    Aula 1

---
+++ aula_1/hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void){
+  int retorno = 0;
+  
+  printf("Hello world!");
+  printf("%c",0xa);
+  printf("%g - %g - %g - %g\n", retorno, 10, 0xa, 'A');
+  return 0;
+}
