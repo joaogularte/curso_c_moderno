99a1881782e1b63e605d1dde3dc5813ac72ae68e
joaogularte <joaogularte@u149326.unisc.br>
2019-09-05 13:40:00 -0300

    Adicionado função que verifica se um pe file

--- readpe/readpe.c
+++ readpe/readpe.c
@@ -11,9 +11,15 @@ void usage () {
   exit(1);
 }
 
+void ispe(const unsigned char *file){
+  if (file[0] != 'M' || file[1] != 'Z'){
+    error("O formato deste arquivo não é PE");
+  }
+}
+
 int main (int argc, char *argv[]) {
   FILE *fh;
-
+  unsigned char content_file[32];
   if (argc != 2) {
     usage();
   }
@@ -23,6 +29,13 @@ int main (int argc, char *argv[]) {
   if (fh == NULL) {
     error("Arquivo nao encontrado ou sem premissão de leitura");
   } 
+  
+  if (fread(content_file, 32, 1, fh) != 1){
+    error("Não consegui ler os 32 bytes do arquivo");
+  }
+  
+  ispe(content_file);
 
+  printf("%c\n", content_file[0]);
   return 0;
 }
