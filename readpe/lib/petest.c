584a29d1d1e58b7df1695cc9fa2bd56b5a0610f8
joaogularte <joaogularte@u149326.unisc.br>
2019-09-13 13:29:55 -0300

    Criado função petest_init

--- readpe/lib/petest.c
+++ readpe/lib/petest.c
@@ -1,5 +1,21 @@
 #include "petest.h"
 
+bool petest_init(PEFILE *pe){
+  FILE *f = fopen(pe->filepath, "rb");
+  if(!f){
+    return false;
+  }
+  pe->hdr_dos = malloc(sizeof(IMAGE_DOS_HEADER));
+  if(!pe->hdr_dos){
+    return false;
+  }
+  
+  fread(pe->hdr_dos, sizeof(IMAGE_DOS_HEADER), 1, f);
+
+  fclose(f);
+  return true;
+}
+
 bool petest_ispe(const unsigned char *file){
   return (file[0] != 'M' || file[1] != 'Z');
 }
