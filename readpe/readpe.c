8277a137a2ed45fbe82ef8a92218a5b5e195cee3
joaogularte <joaogularte@u149326.unisc.br>
2019-09-18 13:06:25 -0300

    :D

--- readpe/readpe.c
+++ readpe/readpe.c
@@ -25,7 +25,11 @@ int main (int argc, char *argv[]) {
   puts(pe.filepath);
   
   petest_init(&pe);
+  if (!petest_ispe(&pe)) {
+    error("Não é um pe");
+  };
   printf("%x\n", pe.header_dos->e_lfanew);
   printf("%zu\n", sizeof(IMAGE_DOS_HEADER));
+  petest_deinit(&pe);
   return 0;
 }
