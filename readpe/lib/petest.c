d5a596537b736d45f6ebd9bc5ebca707ae3268e3
joaogularte <jov96@hotmail.com>
2019-10-09 13:41:26 -0300

    Add some to organize the petest_init method

--- readpe/lib/petest.c
+++ readpe/lib/petest.c
@@ -1,23 +1,29 @@
 #include "petest.h"
 
 bool petest_init(PEFILE *pe){
+
+  //Open de file
   FILE *f = fopen(pe->filepath, "rb");
   if(!f){
     return false;
   }
   
+  //alloc memory to header_dos
   pe->pe_format.header_dos = malloc(sizeof(IMAGE_DOS_HEADER));
   if(!pe->pe_format.header_dos){
     return false;
   }
   
+  //read to header_dos
   fread(pe->pe_format.header_dos, sizeof(IMAGE_DOS_HEADER), 1, f);
   
-
+  //set the position of stream
   fseek(f, pe->pe_format.header_dos->e_lfanew, SEEK_SET);
-  printf("%x\n", ftell(f));
+  
+  //read to signature field
   fread(&(pe->pe_format.signature), sizeof(pe->pe_format.signature), 1, f);
-  //fclose(f);
+  
+  fclose(f);
   return true;
 }
 
